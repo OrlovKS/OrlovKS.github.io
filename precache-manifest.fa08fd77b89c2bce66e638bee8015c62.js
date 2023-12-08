2ac401c3876cc572898b35e7b00f7422385013b4
OrlovKS <kys.tok@ya.ru>
2023-12-08 19:53:09 +0300

    update

---
+++ precache-manifest.fa08fd77b89c2bce66e638bee8015c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5958e16dcd331f88f2ac3411d144f5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2cfa6e3b42af48997f72",
+    "url": "./static/css/2.75bc9036.chunk.css"
+  },
+  {
+    "revision": "cb472688811de7079cf4",
+    "url": "./static/css/main.a8f0f639.chunk.css"
+  },
+  {
+    "revision": "2cfa6e3b42af48997f72",
+    "url": "./static/js/2.50fccbe7.chunk.js"
+  },
+  {
+    "revision": "cb472688811de7079cf4",
+    "url": "./static/js/main.11cc01b4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
