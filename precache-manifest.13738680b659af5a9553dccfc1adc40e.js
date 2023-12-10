5bcec7b857b6c89154b72034848122952c514f00
OrlovKS <kys.tok@ya.ru>
2023-12-10 23:58:55 +0300

    fix url

---
+++ precache-manifest.13738680b659af5a9553dccfc1adc40e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f3b128def919b7834f4beb35933dc3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c37f18cde114b20e9118",
+    "url": "./static/css/2.75bc9036.chunk.css"
+  },
+  {
+    "revision": "26b41268d07431a79120",
+    "url": "./static/css/main.a8f0f639.chunk.css"
+  },
+  {
+    "revision": "c37f18cde114b20e9118",
+    "url": "./static/js/2.fe8df8cd.chunk.js"
+  },
+  {
+    "revision": "87f1300fb9c0b6250f19345d2430586e",
+    "url": "./static/js/2.fe8df8cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b41268d07431a79120",
+    "url": "./static/js/main.aaad812c.chunk.js"
+  },
+  {
+    "revision": "52149e4e1d259529e6a3",
+    "url": "./static/js/runtime-main.701308a8.js"
+  }
+]);
